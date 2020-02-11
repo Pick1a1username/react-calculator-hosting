e94db10eb5e6ee289785fb88c538c6cfee66bec8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-11 07:48:54 +0000

    Deploy pick1a1username/react-calculator-hosting to github.com/pick1a1username/react-calculator-hosting.git:master

---
+++ precache-manifest.6d7ea9487bb829d6b196533611b12253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f623173973b546c2eb293f54f1255cd4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74914a2838cd644f2615",
+    "url": "./static/css/main.101155db.chunk.css"
+  },
+  {
+    "revision": "baece9ed6915c573a4a1",
+    "url": "./static/js/2.6ba2822b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.6ba2822b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74914a2838cd644f2615",
+    "url": "./static/js/main.11d521dc.chunk.js"
+  },
+  {
+    "revision": "f4868e2a688472e781cf",
+    "url": "./static/js/runtime-main.33a3595b.js"
+  }
+]);
